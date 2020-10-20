ba3176be15cb0ebcdb2fdd3977f33f464bbed2f9
i has candys <ichen10@stuy.edu>
2020-10-20 18:10:37 -0400

    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+	public static void printLoop(int n){
+  		for(int i = 1; i <= n; i++) {
+    		for(int j = n - i; j >= 0; j--) {
+       			System.out.print(i);
+    		}
+    		System.out.println();
+  		}
+	}
+
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			printLoop(Integer.parseInt(args[0]));
+		}
+		else {
+			printLoop(5);
+		}
+	}
+}
